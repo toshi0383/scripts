2bcb721ddab2586490093b9979717e3494026f09
toshi0383 <t.suzuki326@gmail.com>
2015-02-26 10:52:28 +0900

    [mod] use env variables

--- setupBashrc.sh
+++ setupBashrc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SETTING_DIR=/Users/$USER/Setting
+
 cat > ~/.bashrc << EOF
 alias ll='ls -l'
 alias lrt='ls -lrt'
@@ -8,21 +10,21 @@ source /usr/local/etc/bash_completion.d/git-prompt.sh
 source /usr/local/etc/bash_completion.d/git-completion.bash
 
 export EDITOR=vi
-export XCODE_FILE_TEMPLATES_DIR="/Users/tsuzuki/Library/Developer/Xcode/Templates/File Templates/Custom"
+export XCODE_FILE_TEMPLATES_DIR="/Users/$USER/Library/Developer/Xcode/Templates/File Templates/Custom"
 
-export ANDROID_HOME=/Users/tsuzuki/Library/Android/sdk/
+export ANDROID_HOME=/Users/$USER/Library/Android/sdk/
 export ANDROID_PLATFORM_TOOLS=$ANDROID_HOME/platform-tools
 export ANDROID_TOOLS=$ANDROID_HOME/tools
 if [ -d $ANDROID_TOOLS ];then
   export PATH=$PATH:$ANDROID_PLATFORM_TOOLS:$ANDROID_TOOLS
 fi
 
-export MAVEN_HOME=/Users/tsuzuki/Setting/apache-maven-3.2.5
+export MAVEN_HOME=$SETTING_DIR/apache-maven-3.2.5
 if [ -d $MAVEN_HOME ];then
   export PATH=$PATH:$MAVEN_HOME/bin
 fi
 
-export JD_CMD_HOME=/Users/tsuzuki/Setting/jd-cmd/
+export JD_CMD_HOME=$SETTING_DIR/jd-cmd/
 if [ -d $JD_CMD_HOME ];then
   export PATH=$PATH:$JD_CMD_HOME/bin
 fi
