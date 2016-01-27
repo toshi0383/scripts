0cff397c223c7b4ee109f915cade792e4fb2ffdd
toshi0383 <t.suzuki326@gmail.com>
2016-01-27 15:33:44 +0900

    Add replaceGitOriginWith.sh

---
+++ replaceGitOriginWith.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ $# -ne 1 ];then
+    echo Please specify new origin URL.
+    exit 1
+fi
+new=$1
+current=`git remote -v | head -1 | awk '{print $2}'`
+git remote add upstream $current
+if [ $? -ne 0 ];then
+    exit $?
+fi
+git remote remove origin
+git remote add origin $new
+git fetch -a
+git remote -v
