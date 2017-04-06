9c7597afc3067e9cdf5b90abecfc4eb96f434162
toshi0383 <t.suzuki326@gmail.com>
2017-04-07 00:36:37 +0900

    Migrated to xcconfig-extractor

--- iOS/xcodeproj/buildSettings/main.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BASEDIR=$(cd $(dirname $0);pwd)
-PROJ=${1:?}
-XCCONFIG_OUT_DIR=${2:-.}
-$BASEDIR/extract-build-settings-to-xcconfigs.sh $PROJ $XCCONFIG_OUT_DIR
-awk -F= -f $BASEDIR/filter-build-settings.awk $PROJ > a && mv a $PROJ
-
