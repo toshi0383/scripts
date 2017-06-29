f34a6b76ce374c8d98f1436db50de03330d4b127
toshi0383 <t.suzuki326@gmail.com>
2017-06-29 14:46:51 +0900

    Fix release.sh to accept dependencies as parameter

--- swiftpm/release.sh
+++ swiftpm/release.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 APP_NAME=${1:?}
+shift
 RELEASE_DIR=.build/release
 EXECUTABLE=${RELEASE_DIR}/${APP_NAME}
-LIBCYAML_DYLIB=${RELEASE_DIR}/libCYaml.dylib
 install_name_tool -delete_rpath `xcode-select -p`/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx $EXECUTABLE
-install_name_tool -change "$(pwd)/.build/release/libCYaml.dylib" "@rpath/libCYaml.dylib" $EXECUTABLE
+mkdir -p usr/local/Frameworks/${APP_NAME}
+for lib in $@
+do
+	install_name_tool -change "$(pwd)/.build/release/${lib}" "@rpath/${lib}" $EXECUTABLE
+	LIB=${RELEASE_DIR}/${lib}
+	mv $LIB usr/local/Frameworks/${APP_NAME}/
+done
 mkdir -p usr/local/bin
 mv $EXECUTABLE usr/local/bin/
-mkdir -p usr/local/Frameworks/${APP_NAME}
-mv $LIBCYAML_DYLIB usr/local/Frameworks/${APP_NAME}/
 zip -r ${APP_NAME}.zip usr
