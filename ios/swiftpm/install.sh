4883ad293c7db35886c7f5d289496ba95ad7a0de
toshi0383 <t.suzuki326@gmail.com>
2017-06-29 11:20:03 +0900

    Fix install.sh and release.sh

--- ios/swiftpm/install.sh
+++ ios/swiftpm/install.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 APP_NAME=${1:?}
-BINARY_URL=$(curl -s https://api.github.com/repos/toshi0383/${APP_NAME}/releases | grep browser_download_url | head -1 | awk -F": \"" '{print $2}' | sed 's/\"//')
+TEMPORARY_FOLDER=/tmp/${APP_NAME}.dst
+rm -rf $TEMPORARY_FOLDER
+mkdir -p $TEMPORARY_FOLDER 2> /dev/null
+CLIENT_ID=6da3e83e315e51292de6
+CLIENT_SECRET=a748acd67f2e95d6098ff29243f415133b055226
+
+BINARY_URL=$(curl -s "https://api.github.com/repos/toshi0383/${APP_NAME}/releases?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}" | grep browser_download_url | head -1 | awk -F": \"" '{print $2}' | sed 's/\"//')
 echo $BINARY_URL
-curl -sLk $BINARY_URL -o $APP_NAME
-chmod +x $APP_NAME
-install_name_tool -add_rpath `xcode-select -p`/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx $APP_NAME
-mv $APP_NAME /usr/local/bin
+
+# Download zip
+cd ${TEMPORARY_FOLDER}
+ZIP_NAME=${APP_NAME}.zip
+curl -sLk $BINARY_URL -o ${ZIP_NAME}
+
+# Install
+unzip ${ZIP_NAME}
+chmod +x usr/local/bin/$APP_NAME
+install_name_tool -add_rpath "@executable_path/../Frameworks/${APP_NAME}" "usr/local/bin/${APP_NAME}"
+install_name_tool -add_rpath "`xcode-select -p`/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx" "usr/local/bin/${APP_NAME}"
+cp -Rf usr /
--- ios/swiftpm/release.sh
+++ ios/swiftpm/release.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 APP_NAME=${1:?}
-EXECUTABLE=.build/release/${APP_NAME}
+RELEASE_DIR=.build/release
+EXECUTABLE=${RELEASE_DIR}/${APP_NAME}
+LIBCYAML_DYLIB=${RELEASE_DIR}/libCYaml.dylib
 install_name_tool -delete_rpath `xcode-select -p`/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx $EXECUTABLE
+install_name_tool -change "$(pwd)/.build/release/libCYaml.dylib" "@rpath/libCYaml.dylib" $EXECUTABLE
+mkdir -p usr/local/bin
+mv $EXECUTABLE usr/local/bin/
+mkdir -p usr/local/Frameworks/${APP_NAME}
+mv $LIBCYAML_DYLIB usr/local/Frameworks/${APP_NAME}/
+zip -r ${APP_NAME}.zip usr
