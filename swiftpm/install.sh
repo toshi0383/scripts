9ed05dd88be8cb991f5f4dfc237867940b8ccd6d
toshi0383 <t.suzuki326@gmail.com>
2017-06-29 17:38:09 +0900

    Accept other repositories than mine and improve doc

--- swiftpm/install.sh
+++ swiftpm/install.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
-APP_NAME=${1:?}
+#
+# name:
+#   install.sh
+#
+# description:
+#   Download latest release from GitHub and install.
+#   Uploaded binary must be created with release.sh.
+#
+# parameters:
+#   1: "user/app" ... your github repository name. (e.g. toshi0383/cmdshelf)
+#
+# author:
+#   Toshihiro Suzuki
+#
+# since:
+#   2017-06-29
+#
+# copyright:
+#   Copyright © 2017 Toshihiro Suzuki All rights reserved.
+#
+
+REPO_NAME=${1:?}
+# Separate an argument by '/'
+OLD_IFS=$IFS; IFS=/; set -- $@; IFS=$OLD_IFS
+# e.g. toshi0383/cmdshelf => cmshelf
+APP_NAME=${2:?}
+
 TEMPORARY_FOLDER=/tmp/${APP_NAME}.dst
 rm -rf $TEMPORARY_FOLDER
 mkdir -p $TEMPORARY_FOLDER 2> /dev/null
 CLIENT_ID=6da3e83e315e51292de6
 CLIENT_SECRET=a748acd67f2e95d6098ff29243f415133b055226
 
-BINARY_URL=$(curl -s "https://api.github.com/repos/toshi0383/${APP_NAME}/releases?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}" | grep browser_download_url | head -1 | awk -F": \"" '{print $2}' | sed 's/\"//')
+# Get binary URL via github api
+BINARY_URL=$(curl -s "https://api.github.com/repos/${REPO_NAME}/releases?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}" | grep browser_download_url | head -1 | awk -F": \"" '{print $2}' | sed 's/\"//')
 echo $BINARY_URL
 
 # Download zip
