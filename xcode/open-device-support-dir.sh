445b63b9c6fb97d684bdb348146af40cf5f90474
toshi0383 <t.suzuki326@gmail.com>
2017-10-02 17:20:11 +0900

    xcode/open-device-support-dir.sh: improve performance

--- xcode/open-device-support-dir.sh
+++ xcode/open-device-support-dir.sh
@@ -2,8 +2,24 @@
 OS=${1:-'[a-z]'}
 for xcode in `ls -d /Applications/Xcode*.app`
 do
-    path=${xcode}/Contents/Developer/Platforms
-    dirs="${dirs} $(find ${path} -d 2 -name DeviceSupport -type d | grep --ignore-case $OS)"
+    case $OS in
+    i[oO][sS])
+        dirs="$dirs ${xcode}/Contents/Developer/Platforms/iPhoneOS.platform/DeviceSupport"
+        ;;
+    tv[oO][sS])
+        dirs="$dirs ${xcode}/Contents/Developer/Platforms/AppleTVOS.platform/DeviceSupport"
+        ;;
+    watch)
+        dirs="$dirs ${xcode}/Contents/Developer/Platforms/WatchOS.platform/DeviceSupport"
+        ;;
+    [mM]ac)
+        dirs="$dirs ${xcode}/Contents/Developer/Platforms/MacOSX.platform/DeviceSupport"
+        ;;
+    *)
+        echo invalid os: $OS
+        exit 1
+        ;;
+    esac
 done
 for dir in $dirs
 do
