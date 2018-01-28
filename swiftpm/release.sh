0c8f46031db68adb99ec4ca2310b2e5a373e6db4
toshi0383 <t.suzuki326@gmail.com>
2018-01-28 18:16:43 +0900

    release.sh: remove workaround code

--- swiftpm/release.sh
+++ swiftpm/release.sh
@@ -11,7 +11,6 @@
 #
 # parameters:
 #   1: executable name ... e.g. cmdshelf
-#   2..: required framework name[s] if needed ... e.g. libCYaml.dylib
 #
 # author:
 #   Toshihiro Suzuki
@@ -24,18 +23,11 @@
 #
 
 APP_NAME=${1:?}
-shift
+
 RELEASE_DIR=.build/release
 EXECUTABLE=${RELEASE_DIR}/${APP_NAME}
 DOC=docs/man
 install_name_tool -delete_rpath `xcode-select -p`/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx $EXECUTABLE
-mkdir -p usr/local/Frameworks/${APP_NAME}
-for lib in $@
-do
-	install_name_tool -change "$(pwd)/.build/release/${lib}" "@rpath/${lib}" $EXECUTABLE
-	LIB=${RELEASE_DIR}/${lib}
-	cp -R $LIB usr/local/Frameworks/${APP_NAME}/
-done
 mkdir -p usr/local/bin
 cp $EXECUTABLE usr/local/bin/
 
