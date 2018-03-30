da2b3adbeb38b33b2347c0ec3a83f5ef5c86d301
toshi0383 <t.suzuki326@gmail.com>
2018-03-30 23:14:01 +0900

    Update bitrise/download-recent-error-logs.sh

--- bitrise/download-recent-error-logs.sh
+++ bitrise/download-recent-error-logs.sh
@@ -8,7 +8,7 @@
 #
 # parameter 1: fetch limit
 #
-LIMIT=${1:-10}
+LIMIT=${1:-1}
 for BUILD_SLUG in $(curl -sS -H "Authorization: token $BITRISE_API_TOKEN" "https://api.bitrise.io/v0.1/apps/${APP_SLUG:?}/builds?status=2&limit=${LIMIT}" | jq '.data | map(.slug)' | gsed -nr -e 's/.*"(.*)".*/\1/p')
 do
     LOG_URL=$(curl -sS -H "Authorization: token $BITRISE_API_TOKEN" "https://api.bitrise.io/v0.1/apps/${APP_SLUG}/builds/${BUILD_SLUG}/log" | jq .expiring_raw_log_url)
