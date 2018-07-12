a9d1cf8459282d25cc6b571383d0d075242e6488
toshi0383 <t.suzuki326@gmail.com>
2018-07-12 11:44:36 +0900

    Update xquick.sh

--- xcode/xquick.sh
+++ xcode/xquick.sh
@@ -2,7 +2,7 @@
 FILE=${1:?}
 if [ $# -eq 2 ]; then
     # Revert
-    COMMAND='s,  xdescribe(.*) // xquick,  describe\1,;s,  xcontext(.*) // xquick,  context\1,;s,  xit(.*) // xquick,  it\1,'
+    COMMAND='s,  xdescribe(.*) // xquick,  describe\1,;s,  xcontext(.*) // xquick,  context\1,;s,  xit(.*) // xquick,  it\1,;s, // xquick,,'
 else
     # Apply
     COMMAND='s,  describe(.*),  xdescribe\1 // xquick,;s,  context(.*),  xcontext\1 // xquick,;s,  it(.*),  xit\1 // xquick,'
