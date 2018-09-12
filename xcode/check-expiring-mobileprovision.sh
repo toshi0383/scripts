ba1c87f70d82ba7eb72abe269a0a00079b73cdfe
toshi0383 <t.suzuki326@gmail.com>
2018-09-12 15:46:07 +0900

    Add xcode/check-expiring-mobileprovision.sh

---
+++ xcode/check-expiring-mobileprovision.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# name:
+#   check-expiring-mobileprovision.sh
+#
+# description:
+#   Search for expiring mobileprovision file and prints them in console.
+#
+# parameter:
+#   - files list
+#
+# author:
+#   鈴木  俊裕
+#
+# dependency:
+#   - gdate: to format date string to unixtime
+#
+# copyright:
+#   Copyright © 2018年 toshi0383 All rights reserved.
+#
+# license:
+#   MIT
+#
+
+if [ $# -eq 0 ];then
+    echo "no files given."
+    echo "  e.g."
+    echo "    $0 \$(ls certs/*mobileprovision)"
+    exit 1
+fi
+
+for m in $@
+do
+    security cms -D -i "$m" > a
+
+    d=$(/usr/libexec/PlistBuddy -c 'Print ExpirationDate' a)
+
+    if [ $(gdate -d "$d" +%s) -lt $(expr $(date +%s) + 43200) ]; then
+        # Expires in less than 30 days
+        echo $m : $d
+    fi
+
+    rm a
+done
