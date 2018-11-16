4f14f2a5271d1fac8f91e7c4b84ec067c5abdbed
toshi0383 <t.suzuki326@gmail.com>
2018-11-16 12:45:25 +0900

    Add fabric/download-ipa.sh

---
+++ fabric/download-ipa.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Usage:
+#   1. Copy URL to your paste board. (e.g. via hands-off api from iPhone)
+#   2. `cmdshelf run fabric/download-ipa.sh`
+#
+PLIST=`mktemp`
+DOWNLOAD_PATH=~/Downloads/downloaded.ipa
+
+if [ -e "$DOWNLOAD_PATH" ];then
+    echo File already exists: "$DOWNLOAD_PATH"
+    exit 1
+fi
+
+curl -L $(curl -s $(pbpaste | gsed -rn 's/.*=(https.*)/\1/p' | cmdshelf run net/url_decode) -o $PLIST; /usr/libexec/PlistBuddy -c 'Print :items:0:assets:0:url' $PLIST; rm $PLIST) -o "${DOWNLOAD_PATH}"
+if [ $? -eq 0 ];then
+    say 'Downloaded an IPA file successfully.'
+else
+    say 'Download failed.'
+fi
